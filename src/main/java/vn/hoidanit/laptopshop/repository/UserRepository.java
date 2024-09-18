cde12746ef81709444f86df2f621c8d2d6ec637c
duyanhzin92 <duyanhz0902@gmail.com>
2024-09-18 14:40:13 +0700

    Update UserRepository.java

--- src/main/java/vn/hoidanit/laptopshop/repository/UserRepository.java
+++ src/main/java/vn/hoidanit/laptopshop/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package vn.hoidanit.laptopshop.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import vn.hoidanit.laptopshop.domain.User;
 import java.util.List;
