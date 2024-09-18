3488d1626845223b2a178b82b8c2faab1bdb8a1c
duyanhzin92 <duyanhz0902@gmail.com>
2024-09-18 09:10:59 +0700

    Update UserController.java

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -9,6 +9,7 @@ import vn.hoidanit.laptopshop.repository.UserRepository;
 import vn.hoidanit.laptopshop.service.UserService;
 
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -34,6 +35,39 @@ public class UserController {
         List<User> users = this.userService.getAllUsers();
         model.addAttribute("users", users);
         return "admin/user/table-user";
+    }
+
+    @RequestMapping("/admin/user/{id}")
+    public String getUserDetailPage(Model model, @PathVariable long id) {
+        User user = this.userService.getUserById(id);
+        model.addAttribute("userDetail", user);
+        return "admin/user/show";
+    }
+
+    @RequestMapping("/admin/user/update/{id}")
+    public String updateUserByIdPage(Model model, @PathVariable long id) {
+        User user = this.userService.getUserById(id);
+        model.addAttribute("newUser", user);
+        return "admin/user/update";
+    }
+
+    @RequestMapping("/admin/user/delete/{id}")
+    public String deleteUserById(Model model, @PathVariable long id) {
+        this.userService.deleteById(id);
+        return "redirect:/admin/user";
+    }
+
+    @RequestMapping(value = "/admin/user/update", method = RequestMethod.POST)
+    public String updateUserPage(Model model, @ModelAttribute("updateUser") User duyanh) {
+        User currentUser = this.userService.getUserById(duyanh.getId());
+        if (currentUser != null) {
+            currentUser.setAddress(duyanh.getAddress());
+            currentUser.setFullName(duyanh.getFullName());
+            currentUser.setPhone(duyanh.getPhone());
+
+            this.userService.handleSaveUser(currentUser);
+        }
+        return "redirect:/admin/user";
     }
 
     @RequestMapping("/admin/user/create")
