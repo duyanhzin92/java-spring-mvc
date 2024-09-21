8398ca17dc0fac35e46e1389dd33aeacf36f6115
duyanhzin92 <duyanhz0902@gmail.com>
2024-09-21 09:09:15 +0700

    Update UserService.java

--- src/main/java/vn/hoidanit/laptopshop/service/UserService.java
+++ src/main/java/vn/hoidanit/laptopshop/service/UserService.java
@@ -2,15 +2,20 @@ package vn.hoidanit.laptopshop.service;
 
 import org.springframework.stereotype.Service;
 import java.util.List;
+
+import vn.hoidanit.laptopshop.domain.Role;
 import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.repository.RoleRepository;
 import vn.hoidanit.laptopshop.repository.UserRepository;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, RoleRepository roleRepository) {
         this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
     }
 
     public User getUserById(long id) {
@@ -32,5 +37,9 @@ public class UserService {
 
     public void deleteById(long id) {
         this.userRepository.deleteById(id);
+    }
+
+    public Role getRoleByName(String name) {
+        return this.roleRepository.findByName(name);
     }
 }
