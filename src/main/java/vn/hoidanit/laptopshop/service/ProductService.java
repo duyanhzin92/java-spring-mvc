ddbaac0aeb8ef5790756d67d50822e47946c7971
duyanhzin92 <duyanhz0902@gmail.com>
2024-09-23 22:35:34 +0700

    .

--- src/main/java/vn/hoidanit/laptopshop/service/ProductService.java
+++ src/main/java/vn/hoidanit/laptopshop/service/ProductService.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import vn.hoidanit.laptopshop.domain.Product;
-import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.repository.ProductRepository;
 
 @Service
