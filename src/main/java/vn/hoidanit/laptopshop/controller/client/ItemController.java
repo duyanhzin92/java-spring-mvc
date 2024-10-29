04f0c94529dbec56d181b0ba3d73c8258372bd15
duyanhzin92 <duyanhz0902@gmail.com>
2024-10-30 00:40:19 +0700

    filter

--- src/main/java/vn/hoidanit/laptopshop/controller/client/ItemController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/client/ItemController.java
@@ -1,12 +1,14 @@
 package vn.hoidanit.laptopshop.controller.client;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,7 +23,9 @@ import vn.hoidanit.laptopshop.domain.Cart;
 import vn.hoidanit.laptopshop.domain.CartDetail;
 import vn.hoidanit.laptopshop.domain.Order;
 import vn.hoidanit.laptopshop.domain.Product;
+import vn.hoidanit.laptopshop.domain.Product_;
 import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.domain.dto.ProductCriteriaDTO;
 import vn.hoidanit.laptopshop.service.OrderService;
 import vn.hoidanit.laptopshop.service.ProductService;
 
@@ -37,24 +41,49 @@ public class ItemController {
     }
 
     @GetMapping("/products")
-    public String getAllProducts(Model model,
-            @RequestParam("page") Optional<String> pageOptional,
-            @RequestParam("name") Optional<String> nameOptional) {
+    public String getProductPage(Model model,
+            ProductCriteriaDTO productCriteriaDTO,
+            HttpServletRequest request) {
         int page = 1;
         try {
-            if (pageOptional.isPresent()) {
-                page = Integer.parseInt(pageOptional.get());
+            if (productCriteriaDTO.getPage().isPresent()) {
+                // convert from String to int
+                page = Integer.parseInt(productCriteriaDTO.getPage().get());
+            } else {
+                // page = 1
             }
         } catch (Exception e) {
+            // page = 1
             // TODO: handle exception
         }
-        String name = nameOptional.isPresent() ? nameOptional.get() : "";
-        Pageable papPageable = PageRequest.of(page - 1, 6);
-        Page<Product> prs = this.productService.getAllProductsWithSpec(papPageable, name);
-        List<Product> listProducts = prs.getContent();
-        model.addAttribute("products", listProducts);
+
+        // check sort price
+        Pageable pageable = PageRequest.of(page - 1, 5);
+
+        if (productCriteriaDTO.getSort() != null && productCriteriaDTO.getSort().isPresent()) {
+            String sort = productCriteriaDTO.getSort().get();
+            if (sort.equals("gia-tang-dan")) {
+                pageable = PageRequest.of(page - 1, 5, Sort.by(Product_.PRICE).ascending());
+            } else if (sort.equals("gia-giam-dan")) {
+                pageable = PageRequest.of(page - 1, 5, Sort.by(Product_.PRICE).descending());
+            }
+        }
+
+        Page<Product> prs = this.productService.fetchProductsWithSpec(pageable, productCriteriaDTO);
+
+        List<Product> products = prs.getContent().size() > 0 ? prs.getContent()
+                : new ArrayList<Product>();
+
+        String qs = request.getQueryString();
+        if (qs != null && !qs.isBlank()) {
+            // remove page
+            qs = qs.replace("page=" + page, "");
+        }
+
+        model.addAttribute("products", products);
         model.addAttribute("currentPage", page);
         model.addAttribute("totalPages", prs.getTotalPages());
+        model.addAttribute("queryString", qs);
         return "client/product/show";
     }
 
--- src/main/java/vn/hoidanit/laptopshop/service/specification/ProductSpecs.java
+++ src/main/java/vn/hoidanit/laptopshop/service/specification/ProductSpecs.java
@@ -1,5 +1,7 @@
 package vn.hoidanit.laptopshop.service.specification;
 
+import java.util.List;
+
 import org.springframework.data.jpa.domain.Specification;
 
 import vn.hoidanit.laptopshop.domain.Product;
@@ -9,4 +11,44 @@ public class ProductSpecs {
     public static Specification<Product> nameLike(String name) {
         return (root, query, criteriaBuilder) -> criteriaBuilder.like(root.get(Product_.NAME), "%" + name + "%");
     }
+
+    public static Specification<Product> minPrice(double minPrice) {
+        return (root, query, criteriaBuilder) -> criteriaBuilder.ge(root.get(Product_.PRICE),
+                minPrice);
+    }
+
+    public static Specification<Product> maxPrice(double maxPrice) {
+        return (root, query, criteriaBuilder) -> criteriaBuilder.le(root.get(Product_.PRICE),
+                maxPrice);
+    }
+
+    public static Specification<Product> factoryLike(List<String> name) {
+        return (root, query, criteriaBuilder) -> criteriaBuilder.in(root.get(Product_.FACTORY)).value(name);
+    }
+
+    public static Specification<Product> matchMultiplePrice(double min, double max) {
+        return (root, query, criteriaBuilder) -> criteriaBuilder.between(root.get(Product_.PRICE), min, max);
+    }
+
+    public static Specification<Product> matchFactory(String factory) {
+        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get(Product_.FACTORY), factory);
+    }
+
+    // case4
+    public static Specification<Product> matchListFactory(List<String> factory) {
+        return (root, query, criteriaBuilder) -> criteriaBuilder.in(root.get(Product_.FACTORY)).value(factory);
+    }
+
+    // case4
+    public static Specification<Product> matchListTarget(List<String> target) {
+        return (root, query, criteriaBuilder) -> criteriaBuilder.in(root.get(Product_.TARGET)).value(target);
+    }
+
+    // case5
+    public static Specification<Product> matchPrice(double min, double max) {
+        return (root, query, criteriaBuilder) -> criteriaBuilder.and(
+                criteriaBuilder.gt(root.get(Product_.PRICE), min),
+                criteriaBuilder.le(root.get(Product_.PRICE), max));
+    }
+
 }
